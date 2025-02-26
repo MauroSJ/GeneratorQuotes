b8fa374f9a9d1ac7126f2181e215d8ab09ce1ab3
MauroSJ <mauro10z@hotmail.com>
2025-02-26 18:55:00 -0300

    add values to html elements  with js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,11 @@
 console.log('funncionando')
+/*-----------SELECTOR DE ELEMENTOS-----------*/
+
+let authorName = document.querySelector(".flex-container__box-author-name")
+let textQuotes = document.querySelector(".flex-container__box-text-quotes")
+
+
+/*-----------PETICION A API-----------*/
 
 // const API_URL = "https://zenquotes.io/api/quotes/  el sevidor de la api no permite peticiones directas (es decir desde el navegador) por eso uso un proxy como intermediario.
 // " 
@@ -12,10 +19,20 @@ async function getQuotes(url) {
         const datos = await response.json()
         console.log(datos);
         const arrayQuotes = JSON.parse(datos.contents)  //convierto la cadena devuelta en array
-        console.log(arrayQuotes[0].q)
+        console.log(arrayQuotes)
+                                        
+        const author = arrayQuotes[0].a
+        const quotes = arrayQuotes[0].q
+
+        authorName.textContent = `${author}`;
+        textQuotes.textContent = `${quotes}`;
+
     } catch (error){
         console.log('Error de la peticion', error)
     }
 }
 
 // getQuotes(API_URL);
+
+
+
